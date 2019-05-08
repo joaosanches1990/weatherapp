8c8c7a65aaa7201ede10a8e6601ee9a9ba9b15e6
joao Sanches <joao.carrelo.sanches@gmail.com>
2019-05-08 19:08:23 +0100

    wheather app

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Sun or Rain</h1>
+        <p> Google the weather info and much more ...</p>
+      </div>
+    );
+  }
+};
+
+export default Titles;
